c667566435603c96ac9da2f65ba0f896d3e14d55
bernddreiss <bd_reiss@gmx.at>
2025-04-10 16:09:13 +0200

    Cleaned up

--- coccinelle/scripts/runCocci.sh
+++ coccinelle/scripts/runCocci.sh
@@ -2,4 +2,5 @@
 mkdir -p ../results
 mkdir -p ../results/$1
 
-spatch --sp-file $1.cocci /usr/local/src/postgresql -very_quiet > ../results/$1/$1.out
+#spatch --sp-file $1.cocci /usr/local/src/postgresql -very_quiet > ../results/$1/$1.out
+spatch --sp-file $1.cocci /usr/local/src/postgresql > ../results/$1/$1.out
