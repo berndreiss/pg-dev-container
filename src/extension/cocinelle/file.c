640f225869bc81766217cfd696c9cf8a4ab7aa7e
bdreiss <bd_reiss@yahoo.de>
2025-04-30 17:03:35 +0200

    Added test files

--- src/extension/cocinelle/file.c
+++ src/extension/cocinelle/file.c
@@ -3,11 +3,14 @@ int foo (int i) {
     return 0; 
 }
 int bar (int a) { 
+    free(a);
     printf("{i}");
     a = 1;
     return 0; 
 }
-int foobar () { 
+int foobar (int a) {
+    if (true)
+	free(a);
     printf("TEST");
     return 0; 
 }
