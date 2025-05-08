88a95625b2abca3c0abea802a842101f77df8fa6
bdreiss <bd_reiss@yahoo.de>
2025-05-08 21:12:10 +0200

    Added script for formatting results for Latex.

---
+++ coccinelle/scripts/formatLatex.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+LATEX_FILE=../results/resultsLatex.tex
+
+echo "\begin{table}[H]
+   \begin{center}
+   \begin{tabularx}{0.6\linewidth}{l|c|c}
+
+      Category & Free & Realloc \\\\
+      \hline
+" > $LATEX_FILE
+
+FREE=$(grep -A 1 "ALL:" ../results/freestats.out | tail -n 1)
+REALLOC=$(grep -A 1 "ALL:" ../results/reallocstats.out | tail -n 1)
+echo "      All Functions& $FREE & $REALLOC \\\\
+" >> $LATEX_FILE
+
+FREE=$(grep -A 1 "^STRICT:" ../results/freestats.out | tail -n 1)
+REALLOC=$(grep -A 1 "^STRICT:" ../results/reallocstats.out | tail -n 1)
+echo "      C1: Strict Functions& $FREE & $REALLOC \\\\
+" >> $LATEX_FILE
+
+FREE=$(grep -A 1 "DEPENDENT:" ../results/freestats.out | tail -n 1)
+REALLOC=$(grep -A 1 "DEPENDENT:" ../results/reallocstats.out | tail -n 1)
+echo "      C2: Dependent Functions& $FREE & $REALLOC \\\\
+" >> $LATEX_FILE
+
+FREE=$(grep -A 1 "EREPORT:" ../results/freestats.out | tail -n 1)
+REALLOC=$(grep -A 1 "EREPORT:" ../results/reallocstats.out | tail -n 1)
+echo "      C3: Error Functions& $FREE & $REALLOC \\\\
+" >> $LATEX_FILE
+
+FREE=$(grep -A 1 "SAME RETURN:" ../results/freestats.out | tail -n 1)
+REALLOC=$(grep -A 1 "SAME RETURN:" ../results/reallocstats.out | tail -n 1)
+echo "      C5: Reassign& $FREE & $REALLOC \\\\
+" >> $LATEX_FILE
+
+FREE=$(grep -A 1 "ALL WITHOUT STRICT, DEPENDENT, AND EREPORT:" ../results/freestats.out | tail -n 1)
+REALLOC=$(grep -A 1 "ALL WITHOUT STRICT, DEPENDENT, AND EREPORT:" ../results/reallocstats.out | tail -n 1)
+echo "      C6: Arbitrary Functions& $FREE & $REALLOC \\\\
+" >> $LATEX_FILE
+
+echo "   \end{tabularx}
+\end{center}
+   \caption{Count of Categories by Function.}
+   \label{tab:categories:overview}
+\end{table}
+" >> $LATEX_FILE
