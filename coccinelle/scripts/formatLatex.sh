6ac574709078b1010a893470bc431193c07f34f6
bernddreiss <bd_reiss@gmx.at>
2025-07-15 15:44:56 +0200

    Automatically extract numbers for categories

--- coccinelle/scripts/formatLatex.sh
+++ coccinelle/scripts/formatLatex.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
 LATEX_FILE=../results/resultsLatex.tex
+LATEX_FILE_NUMBERS=../results/resultsLatexNumbers.tex
 STATS_FILE_FREE=../results/collected/stats_free.out
 STATS_FILE_REALLOC=../results/collected/stats_realloc.out
 CATEGORY=C
 COUNT=1
+
+if [[ -f $LATEX_FILE_NUMBERS ]]; then
+  rm $LATEX_FILE_NUMBERS
+fi
+
 echo "\begin{table}[H]
    \begin{center}
    \begin{tabularx}{0.6\linewidth}{l|c|c}
@@ -17,23 +23,31 @@ FREE=$(grep -A 1 "ALL:" $STATS_FILE_FREE | tail -n 1)
 REALLOC=$(grep -A 1 "ALL:" $STATS_FILE_REALLOC | tail -n 1)
 echo "      All Functions& $FREE & $REALLOC \\\\
 " >> $LATEX_FILE
+echo "\\newcommand{\\freeall}{$FREE}" >> $LATEX_FILE_NUMBERS
+echo "\\newcommand{\\reallocall}{$REALLOC}" >> $LATEX_FILE_NUMBERS
 
 FREE=$(grep -A 1 "ALL WITHOUT STATIC:" $STATS_FILE_FREE | tail -n 1)
 REALLOC=$(grep -A 1 "ALL WITHOUT STATIC:" $STATS_FILE_REALLOC | tail -n 1)
 echo "      All without static& $FREE & $REALLOC \\\\
 " >> $LATEX_FILE
+echo "\\newcommand{\\freenostatic}{$FREE}" >> $LATEX_FILE_NUMBERS
+echo "\\newcommand{\\reallocnostatic}{$REALLOC}" >> $LATEX_FILE_NUMBERS
 
 FREE=$(grep -A 1 "^STRICT:" $STATS_FILE_FREE | tail -n 1)
 REALLOC=$(grep -A 1 "^STRICT:" $STATS_FILE_REALLOC | tail -n 1)
 echo "      $CATEGORY$COUNT: Strict Functions& $FREE & $REALLOC \\\\
 " >> $LATEX_FILE
 COUNT=$((COUNT+1))
+echo "\\newcommand{\\freestrict}{$FREE}" >> $LATEX_FILE_NUMBERS
+echo "\\newcommand{\\reallocstrict}{$REALLOC}" >> $LATEX_FILE_NUMBERS
 
 FREE=$(grep -A 1 "^DEPENDENT:" $STATS_FILE_FREE | tail -n 1)
 REALLOC=$(grep -A 1 "^DEPENDENT:" $STATS_FILE_REALLOC | tail -n 1)
 echo "      $CATEGORY$COUNT: Dependent Functions& $FREE & $REALLOC \\\\
 " >> $LATEX_FILE
 COUNT=$((COUNT+1))
+echo "\\newcommand{\\freedependent}{$FREE}" >> $LATEX_FILE_NUMBERS
+echo "\\newcommand{\\reallocdependent}{$REALLOC}" >> $LATEX_FILE_NUMBERS
 
 #FREE=$(grep -A 1 "^EREPORT:" $STATS_FILE_FREE | tail -n 1)
 #REALLOC=$(grep -A 1 "^EREPORT:" $STATS_FILE_REALLOC | tail -n 1)
@@ -47,18 +61,24 @@ REALLOC=$(grep -A 1 "ARBITRARY:" $STATS_FILE_REALLOC | tail -n 1)
 echo "      $CATEGORY$COUNT: Arbitrary Functions& $FREE & $REALLOC \\\\
 " >> $LATEX_FILE
 COUNT=$((COUNT+1))
+echo "\\newcommand{\\freearbitrary}{$FREE}" >> $LATEX_FILE_NUMBERS
+echo "\\newcommand{\\reallocarbitrary}{$REALLOC}" >> $LATEX_FILE_NUMBERS
 
 FREE=$(grep -A 1 "SAME RETURN:" $STATS_FILE_FREE | tail -n 1)
 REALLOC=$(grep -A 1 "SAME RETURN:" $STATS_FILE_REALLOC | tail -n 1)
 echo "      $CATEGORY$COUNT: Functions to Reassign& $FREE & $REALLOC \\\\
 " >> $LATEX_FILE
 COUNT=$((COUNT+1))
+echo "\\newcommand{\\freesamereturn}{$FREE}" >> $LATEX_FILE_NUMBERS
+echo "\\newcommand{\\reallocsamereturn}{$REALLOC}" >> $LATEX_FILE_NUMBERS
 
 FREE=$(grep -A 1 "BOOL RETURN:" $STATS_FILE_FREE | tail -n 1)
 REALLOC=$(grep -A 1 "BOOL RETURN:" $STATS_FILE_REALLOC | tail -n 1)
 echo "      $CATEGORY$COUNT: Functions Returning Boolean& $FREE & $REALLOC \\\\
 " >> $LATEX_FILE
 COUNT=$((COUNT+1))
+echo "\\newcommand{\\freebool}{$FREE}" >> $LATEX_FILE_NUMBERS
+echo "\\newcommand{\\reallocbool}{$REALLOC}" >> $LATEX_FILE_NUMBERS
 
 echo "   \end{tabularx}
 \end{center}
