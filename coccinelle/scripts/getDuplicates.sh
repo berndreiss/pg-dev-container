6a3b7b4e1ef0a0d6fefba61c17bb987dd6d52a55
bernddreiss <bd_reiss@gmx.at>
2025-07-15 22:21:52 +0200

    Fixed typo and added duplicate detection for realloc

--- coccinelle/scripts/getDuplicates.sh
+++ coccinelle/scripts/getDuplicates.sh
@@ -6,7 +6,14 @@ cat $RESULTS_PATH | grep ">" | cut -d "," -f 1 | sort | uniq -d > ../results/all
 while IFS= read -r line; do
   functionname=$(echo $line | cut -d "," -f 1)
   if grep -Fxq "$functionname" ../results/allDuplicate.out; then
-    echo "$line" >> ../results/dulicates.out
+    echo "$line" >> ../results/duplicatesFree.out
   fi
 done < ../results/free/functionnamesonlywithoutstatic.out
+
+while IFS= read -r line; do
+  functionname=$(echo $line | cut -d "," -f 1)
+  if grep -Fxq "$functionname" ../results/allDuplicate.out; then
+    echo "$line" >> ../results/duplicatesRealloc.out
+  fi
+done < ../results/realloc/functionnamesonlywithoutstatic.out
 
